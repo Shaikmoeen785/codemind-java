e4b47b2265c278b7261ec843bbc86952365c499f
Shaikmoeen785 <119731357+Shaikmoeen785@users.noreply.github.com>
2023-08-18 12:15:41 +0530

    import java.util.Scanner; public class PN {     public static void main(String[] args)     {         Scanner scanner=new Scanner(System.in);         int n=scanner.nextInt();         int c=0;         for (int i=1;i<=n;i++)         {             if(n%i==0)             {                 c++;             }         }
            if (c==2)
            {
                System.out.println("Prime");
            }
            else 
            {
                System.out.println("Not Prime");
            }
        }
    }

---
+++ Prime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class PN
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int c=0;
+        for (int i=1;i<=n;i++) 
+        {
+            if(n%i==0) 
+            {
+                c++;
+            }
+        }
+        
+        if (c==2)
+        {
+            System.out.println("Prime");
+        }
+        else 
+        {
+            System.out.println("Not Prime");
+        }
+    }
+}
